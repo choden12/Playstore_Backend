37b25601f3b2c0cd2b0999c58c248e67352edf84
choden12 <sangaycho27@gmail.com>
2025-05-12 15:42:58 +0530

    wip : api integration

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { PrismaClient } from '../generated/prisma/index.js'
+
+const prisma = new PrismaClient()
 
 const app = new Hono()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+
+app.get('/games', async (c) => {
+  const games = await prisma.games.findMany()
+  return c.json(games)
+})
+
 
 serve({
   fetch: app.fetch,
